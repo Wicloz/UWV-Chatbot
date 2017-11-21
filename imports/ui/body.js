d5be1b8acc0eba6a0a2457277542504cb4133b83
Wilco de Boer <wfh@wilcodeboer.me>
2017-11-22 00:02:52 +0100

    Super simpele bot

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,6 +1,7 @@
 import './body.html';
 import { Messages } from "../api/messages";
 import { Conversations } from "../api/conversations";
+import { Answers } from "../api/answers";
 
 // Functions
 function conversation() {
@@ -58,9 +59,18 @@ Template.body.events({
 
     // Send bot message
     if (conversation().withBot) {
+      const answerObject = Answers.findOne({
+        question: text
+      });
+
+      let answerText = "Sorry, dat begreep ik niet, probber iets anders te vragen.";
+      if (typeof answerObject !== "undefined") {
+        answerText = answerObject.answer;
+      }
+
       Messages.insert({
         conversationId: Session.get("ConversationId"),
-        content: 'I am a bot!',
+        content: answerText,
         fromUser: false,
         timeSent: new Date()
       });
